2b929de6acb99f00cebdd18d96416045a35bdb22
unknown <tharindu.samare123@gmail.com>
2022-04-13 23:56:40 +0530

    improvement: read json file external to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:external_path/external_path.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,139 +10,110 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Startup Name Generator',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white10,
-          foregroundColor: Colors.black,
-        ),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home:const RandomWords(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};     // NEW
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class _MyHomePageState extends State<MyHomePage> {
+  // int _counter = 0;
+  List _items = [];
+
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    String path = await ExternalPath.getExternalStoragePublicDirectory(ExternalPath.DIRECTORY_DOWNLOADS);
+    File file = await File('$path/fyp/sample.json');
+    String contents = await file.readAsString();
+    final data = await json.decode(contents);
+    setState(() {
+      _items = data["items"];
+    });
+  }
+
+  // void _fetchData() async {
+  //   String path = await ExternalPath.getExternalStoragePublicDirectory(
+  //       ExternalPath.DIRECTORY_DOWNLOADS);
+  //   print(path);
+  //   File file = await File('$path/fyp/sample.json');
+  //   String contents = await file.readAsString();
+  //   print(contents);
+  //
+  //   // setState(() {
+  //   //   _counter++;
+  //   // });
+  // }
+
+  void requestPermission() {
+    // PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    Permission.storage.request();
+  }
+
+  @override
+  void initState() {
+    requestPermission();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(
-              onPressed: _pushSaved,
-              tooltip: 'Saved Suggestions',
-              icon: const Icon(Icons.list),
-          )
-        ],
+        title: Text(widget.title),
       ),
-      // body:  _buildSuggestions(),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider();
-          /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          final alreadySaved = _saved.contains(_suggestions[index]);
-          return ListTile(
-            // _buildRow(_suggestions[index])
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont,
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Load Data'),
+              onPressed: readJson,
             ),
-            trailing: Icon(
-              alreadySaved ? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved ? Colors.red : null,
-              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
 
-            ),
-            onTap: (){
-              setState(() {
-                if (alreadySaved) {
-                  _saved.remove(_suggestions[index]);
-                } else {
-                  _saved.add(_suggestions[index]);
-                }
-              });
-            },
-          );
-        },
+            // Display the data loaded from sample.json
+            _items.isNotEmpty
+                ? Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Text(_items[index]["id"]),
+                      title: Text(_items[index]["name"]),
+                      subtitle: Text(_items[index]["description"]),
+                    ),
+                  );
+                },
+              ),
+            )
+                : Container()
+          ],
+        ),
       ),
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _fetchData,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
     );
   }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      // Add lines from here...
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-                (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ), // ...to here.
-    );
-  }
-//   Widget _buildRow(WordPair pair) {
-//     final alreadySaved = _saved.contains(pair);
-//     return ListTile(
-//       title: Text(
-//         pair.asPascalCase,
-//         style: _biggerFont,
-//       ),
-//       trailing: Icon(     // NEW from here...
-//         alreadySaved ? Icons.favorite : Icons.favorite_border,
-//         color: alreadySaved ? Colors.red : null,
-//         semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-//       ),
-//       onTap: () {      // NEW lines from here...
-//         setState(() {
-//           if (alreadySaved) {
-//             _saved.remove(pair);
-//           } else {
-//             _saved.add(pair);
-//           }
-//         });
-//       }, // ... to here.
-//     );
-//   }
 }
